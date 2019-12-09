58b181ead07cb5ccc32c042809bc4f9306bc0d7f
kheabat25 <kheabat@gmail.com>
2019-12-09 11:28:07 +0800

    Comment Display & Delete

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/posts'>Posts</Link>
       </li>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <Link onClick={logout} to='/'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
